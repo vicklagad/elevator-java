ca241aeb22a602a2badad941d475049d6ba6414f
vicklagad <vicklagad@vicks-air.attlocal.net>
2016-03-10 21:41:01 -0600

    Restarting work..

--- src/main/java/kuali/poc/Elevator.java
+++ src/main/java/kuali/poc/Elevator.java
@@ -4,7 +4,6 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 import java.util.TreeSet;
 
 /** The main elevator class **/
